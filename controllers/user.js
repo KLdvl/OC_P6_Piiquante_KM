fa276cfc6888611ca9186bd0633f05d080ec42ab
Kevin <maskevin27@gmail.com>
2022-04-25 15:13:12 +0200

    User login route & signup

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,37 @@
 const User = require("../models/User");
 
 exports.signUp = (req, res, next) => {
-  const user = new User({
-    ...req.body,
-  });
-  user
-    .save()
-    .then(() => res.status(201).json({ message: "Utilisateur créé!" }))
-    .catch((error) => res.status(400).json({ error }));
-  console.log("post signup");
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé!" }))
+        .catch((error) => res.status(400).json({ error }));
+      console.log("post signup");
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
 exports.logIn = (req, res, next) => {
-  console.log("post login");
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe invalide" });
+          }
+          return res.status(200).json({ userId: user._id, token: "TOKEN" });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
