8f866ca744d5a20c5628a253611aae99c14aee75
Kevin <maskevin27@gmail.com>
2022-05-11 21:26:52 +0200

    Comments in like/dislike controller

--- controllers/likes/like.js
+++ controllers/likes/like.js
@@ -1,6 +1,6 @@
 const Sauce = require("../../models/Sauce");
 
-// Method for liking / disliking a sauce
+// Method for liking a sauce
 exports.likeSauce = (req, res, next) => {
   if(req.body.like === 1) {
     return Sauce.updateOne({_id: req.params.id}, {$inc: {likes: +1}, $push: {usersLiked: req.body.userId}})
@@ -8,19 +8,25 @@ exports.likeSauce = (req, res, next) => {
       .catch(error => res.status(400).json({error}))
   }
 
+  // Method for disliking a sauce
   if (req.body.like === -1) {
     return Sauce.updateOne({_id: req.params.id}, {$inc: {dislikes: +1}, $push: {usersDisliked: req.body.userId}})
       .then(() => res.status(200).json({message: "Je dislike cette sauce"}))
       .catch(error => res.status(400).json({error}))
   }
 
+  // Method when sauce are already liked or disliked
   Sauce.findOne({_id: req.params.id})
       .then(sauce => {
+
+        // Method to withdraw like when sauce is liked
         if(sauce.usersLiked.includes(req.body.userId)) {
           return Sauce.updateOne({_id: req.params.id}, {$inc: {likes: -1}, $pull: {usersLiked: req.body.userId}})
             .then(() => res.status(200).json({message: "Je retire mon like"}))
             .catch(error => res.status(400).json({error}))
         }
+
+        // Method to withdraw dislike when sauce is disliked
         if (sauce.usersDisliked.includes(req.body.userId)) {
           return Sauce.updateOne({_id: req.params.id}, {$inc: {dislikes: -1}, $pull: {usersDisliked: req.body.userId}})
             .then(() => res.status(200).json({message: "Je retire mon dislike"}))
--- routes/sauce.js
+++ routes/sauce.js
@@ -7,18 +7,18 @@ const multer = require("../middleware/multer-config");
 const { sauceValidationRules, validate } = require("../middleware/validator");
 
 // Importing methods for sauce
-const sauceCtrl = require("../controllers/sauce");
+const sauceController = require("../controllers/sauce");
 const sauceControllerLike = require("../controllers/likes/like");
 
 // Creating express Router
 const router = express.Router();
 
 // Routing for sauces
-router.get("/", auth, sauceCtrl.getSauces);
-router.get("/:id", auth, sauceCtrl.getOneSauce);
-router.post("/", auth, sauceValidationRules(), validate, multer, sauceCtrl.createSauce);
-router.put("/:id", auth, sauceValidationRules(), validate, multer, sauceCtrl.modifySauce);
-router.delete("/:id", auth, multer, sauceCtrl.deleteSauce);
+router.get("/", auth, sauceController.getSauces);
+router.get("/:id", auth, sauceController.getOneSauce);
+router.post("/", auth, sauceValidationRules(), validate, multer, sauceController.createSauce);
+router.put("/:id", auth, sauceValidationRules(), validate, multer, sauceController.modifySauce);
+router.delete("/:id", auth, multer, sauceController.deleteSauce);
 router.post("/:id/like", auth, sauceControllerLike.likeSauce);
 
 module.exports = router;
