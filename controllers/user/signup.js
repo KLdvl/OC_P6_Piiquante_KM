563692d9c3932c226500d315e9724cf7138af0d9
Kevin <maskevin27@gmail.com>
2022-05-16 15:57:32 +0200

    Crypting email in signUp route

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -1,22 +1,28 @@
 // External requires
 const bcrypt = require("bcrypt");
+const cryptojs = require("crypto-js")
 
 // Model used
 const User = require("../../models/User");
 
 // Method for signing up with password hashing with bcrypt
 exports.signUp = (req, res) => {
+  //Crypting email
+  const emailCrypted = cryptojs.HmacSHA256(req.body.email, process.env.SECRET_KEY).toString();
+
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
       const user = new User({
-        email: req.body.email,
+        email: emailCrypted,
         password: hash,
       });
+
+      // Saving user to database
       user
         .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé!" }))
-        .catch(error => res.status(400).json({ error }));
+        .then(() => res.status(201).json({ message: "Utilisateur créé" }))
+        .catch(error => res.status(400).json({error}).send(console.log(error)));
     })
-    .catch((error) => res.status(500).json({ error }));
+    .catch(error => res.status(500).json({error}).send(console.log(error)));
 };
