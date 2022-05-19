ca333a80406ebfcf8d72f5eab676eb3af18411c5
Kevin <maskevin27@gmail.com>
2022-05-19 23:29:17 +0200

    updated formValidator

--- middleware/validators/formValidator.js
+++ middleware/validators/formValidator.js
@@ -1,19 +1,45 @@
-module.exports = (req, res, next) => {
+// module.exports = (req, res, next) => {
+//
+//   const fieldsRegex = /^[a-zA-Z0-9 _.,!()&]+$/;
+//
+//   const {name, manufacturer, description, mainPepper} = JSON.parse(req.body.sauce);
+//
+//   if(
+//     fieldsRegex.test(name) &&
+//     fieldsRegex.test(manufacturer) &&
+//     fieldsRegex.test(description) &&
+//     fieldsRegex.test(mainPepper)
+//   ) {
+//   next();
+//   } else {
+//     return res
+//       .status(500)
+//       .json({ error: "Certains champs contiennent des caractères invalides" });
+//   }
+// }
+// External requires
+const fs = require("fs");
 
-  const fieldsRegex = /^[a-zA-Z0-9 _.,!()&]+$/;
+module.exports = async (req, res, next) => {
+  try {
 
-  const {name, manufacturer, description, mainPepper} = JSON.parse(req.body.sauce);
+    const fieldsRegex = /^[a-zA-Z0-9 _.,!()&]+$/;
 
-  if(
-    fieldsRegex.test(name) &&
-    fieldsRegex.test(manufacturer) &&
-    fieldsRegex.test(description) &&
-    fieldsRegex.test(mainPepper)
-  ) {
-  next();
-  } else {
-    return res
-      .status(500)
-      .json({ error: "Certains champs contiennent des caractères invalides" });
+    const {name, manufacturer, description, mainPepper} = await JSON.parse(req.body.sauce);
+      if(fieldsRegex.test(name) &&
+      fieldsRegex.test(manufacturer) &&
+      fieldsRegex.test(description) &&
+      fieldsRegex.test(mainPepper)) {
+        next();
+      } else {
+        fs.unlink(`images/${req.file.filename}`, (err) => {
+          if (err) {
+            throw err;
+          }
+          res.status(500).json({message: "Certains champs contiennent des caractères invalides"})
+        });
+      }
+  } catch(err) {
+    res.status(500).json({message: "Certains champs contiennent des caractères invalides"})
   }
 }
