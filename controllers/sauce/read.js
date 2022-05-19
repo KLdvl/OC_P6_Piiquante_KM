5332f9a851f81e92a4eba01f6acb77acc2f7da57
Kevin <maskevin27@gmail.com>
2022-05-19 20:59:27 +0200

    Last modification read controller

--- controllers/sauce/read.js
+++ controllers/sauce/read.js
@@ -2,10 +2,6 @@
 const Sauce = require("../../models/Sauce");
 
 // Method for getting all the sauces
-
-
-
-
 exports.readSauces = async (req, res) => {
   try {
     const sauce = await Sauce.find({})
@@ -15,22 +11,12 @@ exports.readSauces = async (req, res) => {
   }
 }
 
-
-
+// Method for getting one sauce selected by Id
 exports.readOneSauce = async (req, res) => {
   try {
-    const sauce = await Sauce.find({_id: req.params.id}).exec();
-
-=======
-// Method for getting one sauce using Id
-
-exports.readOneSauce = async (req, res) => {
-  try {
-    const sauce = await Sauce.findOne({_id: req.params.id}).exec();
->>>>>>> 0c0a4d367c9b04c2b1abf72576d25a15bf87cd44
+    const sauce = await Sauce.findById({_id: req.params.id}).exec();
     res.status(200).json(sauce)
   } catch(err) {
     res.status(404).json({error: err})
   }
 }
-
