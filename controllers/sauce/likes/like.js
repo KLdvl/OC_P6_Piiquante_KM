787777247594b29688f27b8cd55fb4ff71f3a323
Kevin <maskevin27@gmail.com>
2022-05-19 22:58:21 +0200

    like & dislike controllers ECMA 2017

--- controllers/sauce/likes/like.js
+++ controllers/sauce/likes/like.js
@@ -1,23 +1,25 @@
 const Sauce = require("../../../models/Sauce");
 
-exports.likeSauce = (req, res, next) => {
-
-  // Method for liking a sauce
-  if(req.body.like === 1) {
-    return Sauce.updateOne({_id: req.params.id}, {$inc: {likes: +1}, $push: {usersLiked: req.body.userId}})
-      .then(() => res.status(200).json({message: "Je like cette sauce"}))
-      .catch(error => res.status(400).json({error}))
+exports.likeSauce = async (req, res, next) => {
+  try {
+    // Destructuring
+    const {like, userId} = req.body;
+    // Creating a like
+    if(like === 1) {
+      await Sauce.findByIdAndUpdate({_id: req.params.id}, {$inc: {likes: +1}, $push: {usersLiked: userId}})
+      res.status(200).json({message: "Je like cette sauce"})
+    }
+    // Withdrawing a like
+    if(like === 0) {
+      const sauce = await Sauce.findById({_id: req.params.id})
+      const {usersLiked} = sauce;
+      if (usersLiked.includes(userId)) {
+        await Sauce.updateOne({_id: req.params.id}, {$inc: {likes: -1}, $pull: {usersLiked: userId}})
+        res.status(200).json({message: "Je retire mon like"})
+      }
+    }
+  } catch(err) {
+    res.status(400).json({error : err})
   }
-
-  Sauce.findOne({_id: req.params.id})
-      .then(sauce => {
-        // Method to withdraw like when sauce is liked
-        if(sauce.usersLiked.includes(req.body.userId)) {
-          return Sauce.updateOne({_id: req.params.id}, {$inc: {likes: -1}, $pull: {usersLiked: req.body.userId}})
-            .then(() => res.status(200).json({message: "Je retire mon like"}))
-            .catch(error => res.status(400).json({error}))
-        }
-      })
-      .catch(error => res.status(400).json({error}))
-  next();
-};
+  next()
+}
