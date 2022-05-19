0c0a4d367c9b04c2b1abf72576d25a15bf87cd44
laura <vongsy.laura@gmail.com>
2022-05-19 12:43:48 +0200

    modified controllers ECMA2017 sauces read

--- controllers/sauce/read.js
+++ controllers/sauce/read.js
@@ -2,11 +2,6 @@
 const Sauce = require("../../models/Sauce");
 
 // Method for getting all the sauces
-// exports.readSauces = (req, res, next) => {
-//   Sauce.find()
-//     .then(sauce => res.status(200).json(sauce))
-//     .catch(error => res.status(400).json({error}));
-// };
 
 exports.readSauces = async (req, res) => {
   try {
@@ -17,17 +12,12 @@ exports.readSauces = async (req, res) => {
   }
 }
 
-// // Method for getting one sauce using Id
-// exports.readOneSauce = (req, res, next) => {
-//   Sauce.findOne({ _id: req.params.id })
-//     .then(sauce => res.status(200).json(sauce))
-//     .catch(error => res.status(404).json({error}));
-// };
+// Method for getting one sauce using Id
 
 exports.readOneSauce = async (req, res) => {
   try {
-    const sauce = await Sauce.findById({_id: req.params.id}).exec();
-    res.status(200).json({sauce})
+    const sauce = await Sauce.findOne({_id: req.params.id}).exec();
+    res.status(200).json(sauce)
   } catch(err) {
     res.status(404).json({error: err})
   }
