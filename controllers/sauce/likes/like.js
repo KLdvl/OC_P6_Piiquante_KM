602ec6a7ff4e050dca5d9c77bcd030f0e8f017a6
Kevin Mas <maskevin27@gmail.com>
2022-06-17 14:44:29 +0200

    comments on like controller

--- controllers/sauce/likes/like.js
+++ controllers/sauce/likes/like.js
@@ -4,14 +4,18 @@ exports.likeSauce = async (req, res) => {
   try{
     // Destructuring
     const {like, userId} = req.body;
+    // Find sauce that needs to be updated
     const sauce = await Sauce.findById({_id: req.params.id})
+    // Using switch to handle all the cases
     switch(like) {
+      // If like === 1
       case 1:
         if(!sauce.usersLiked.includes(userId)) {
           await Sauce.findByIdAndUpdate({_id: req.params.id}, {$inc: {likes: +1}, $push: {usersLiked: userId}})
           res.status(200).json({message: "Je like cette sauce"})
         }
             break;
+        // If like === 0
       case 0:
         if(sauce.usersLiked.includes(userId)) {
           await Sauce.findByIdAndUpdate({_id: req.params.id}, {$inc: {likes: -1}, $pull: {usersLiked: userId}})
@@ -22,6 +26,7 @@ exports.likeSauce = async (req, res) => {
           res.status(200).json({message: "Je retire mon dislike"})
         }
             break;
+        // If like === -1
       case -1:
         if(!sauce.usersDisliked.includes(userId)) {
           await Sauce.findByIdAndUpdate({_id: req.params.id}, {$inc: {dislikes: +1}, $push: {usersDisliked: userId}})
